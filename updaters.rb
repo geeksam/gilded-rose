5afec4cbe5eb51827e6c1ec8c50352a0bcc78e50
Sam Livingston-Gray <geeksam@gmail.com>
2022-08-18 13:06:45 -0700

    [TCR] That's a long way to say 'zero' ;P

--- updaters.rb
+++ updaters.rb
@@ -20,7 +20,7 @@ class BackstagePass < Updater
     item.quality += 1 if item.sell_in < 6
     item.sell_in -= 1
     if item.sell_in < 0
-      item.quality = item.quality - item.quality
+      item.quality = 0
     end
     trim_quality
   end
