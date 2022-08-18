2ed12a13eee762ed403506260b52c8ec4ff2a39c
Sam Livingston-Gray <geeksam@gmail.com>
2022-08-18 13:06:26 -0700

    [TCR] Switch to postfix 'if' for clarity

--- updaters.rb
+++ updaters.rb
@@ -8,9 +8,7 @@ class AgedBrie < Updater
   def call
     item.quality += 1
     item.sell_in -= 1
-    if item.sell_in < 0
-      item.quality += 1
-    end
+    item.quality += 1 if item.sell_in < 0
     trim_quality
   end
 end
@@ -18,12 +16,8 @@ end
 class BackstagePass < Updater
   def call
     item.quality += 1
-    if item.sell_in < 11
-      item.quality += 1
-    end
-    if item.sell_in < 6
-      item.quality += 1
-    end
+    item.quality += 1 if item.sell_in < 11
+    item.quality += 1 if item.sell_in < 6
     item.sell_in -= 1
     if item.sell_in < 0
       item.quality = item.quality - item.quality
@@ -36,9 +30,7 @@ class NormalItem < Updater
   def call
     item.quality -= 1
     item.sell_in -= 1
-    if item.sell_in < 0
-      item.quality -= 1
-    end
+    item.quality -= 1 if item.sell_in < 0
     trim_quality
   end
 end
