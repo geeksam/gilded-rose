e9b8038f28a202576f14ea6849f7c5074bf81c09
Sam Livingston-Gray <geeksam@gmail.com>
2022-08-18 13:07:36 -0700

    [TCR] Reorder sell_in, quality adjustments in 2/3 places

--- updaters.rb
+++ updaters.rb
@@ -6,8 +6,8 @@ end
 
 class AgedBrie < Updater
   def call
-    item.quality += 1
     item.sell_in -= 1
+    item.quality += 1
     item.quality += 1 if item.sell_in < 0
     trim_quality
   end
@@ -26,8 +26,8 @@ end
 
 class NormalItem < Updater
   def call
-    item.quality -= 1
     item.sell_in -= 1
+    item.quality -= 1
     item.quality -= 1 if item.sell_in < 0
     trim_quality
   end
