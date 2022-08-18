a6e880e856e23fc9fc48c2a6c0dbfd345e060d84
Sam Livingston-Gray <geeksam@gmail.com>
2022-08-18 13:08:07 -0700

    [TCR] Reorder sell_in/quality adjustments, avoid off-by-ones

--- updaters.rb
+++ updaters.rb
@@ -15,10 +15,10 @@ end
 
 class BackstagePass < Updater
   def call
-    item.quality += 1
-    item.quality += 1 if item.sell_in < 11
-    item.quality += 1 if item.sell_in < 6
     item.sell_in -= 1
+    item.quality += 1
+    item.quality += 1 if item.sell_in < 10
+    item.quality += 1 if item.sell_in < 5
     item.quality = 0 if item.sell_in < 0
     trim_quality
   end
