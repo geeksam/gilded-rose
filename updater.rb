527661588d53d99ec81d05f2d377e7041ad420bd
Sam Livingston-Gray <geeksam@gmail.com>
2022-08-18 12:53:45 -0700

    [TCR] -m Remove Sulfuras logic from Updater

--- updater.rb
+++ updater.rb
@@ -24,9 +24,7 @@ class Updater
   def call
     if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
       if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
+        item.quality -= 1
       end
     else
       if item.quality < 50
@@ -45,16 +43,12 @@ class Updater
         end
       end
     end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
+    item.sell_in -= 1
     if item.sell_in < 0
       if item.name != "Aged Brie"
         if item.name != 'Backstage passes to a TAFKAL80ETC concert'
           if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
+            item.quality -= 1
           end
         else
           item.quality = item.quality - item.quality
