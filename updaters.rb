dacea2f77a42a4deb9b6bcf171f3fd78635378ed
Sam Livingston-Gray <geeksam@gmail.com>
2022-08-18 13:14:01 -0700

    [TCR] Extract Method

--- updaters.rb
+++ updaters.rb
@@ -29,7 +29,14 @@ end
 
 class NormalItem < Updater
   def change_quality
-    item.quality -= 1
-    item.quality -= 1 if item.sell_in < 0
+    item.quality += quality_adjustment
+  end
+
+  def quality_adjustment
+    if item.sell_in < 0
+      -2
+    else
+      -1
+    end
   end
 end
