f8406a17b19c0e72adca68e1fffdd911168d559d
Sam Livingston-Gray <geeksam@gmail.com>
2022-08-18 12:56:04 -0700

    [TCR] Remove brie logic from Updater

--- updater.rb
+++ updater.rb
@@ -23,7 +23,7 @@ class Updater
   end
 
   def call
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
+    if item.name != 'Backstage passes to a TAFKAL80ETC concert'
       if item.quality > 0
         item.quality -= 1
       end
@@ -46,18 +46,12 @@ class Updater
     end
     item.sell_in -= 1
     if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            item.quality -= 1
-          end
-        else
-          item.quality = item.quality - item.quality
+      if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+        if item.quality > 0
+          item.quality -= 1
         end
       else
-        if item.quality < 50
-          item.quality += 1
-        end
+        item.quality = item.quality - item.quality
       end
     end
   end
