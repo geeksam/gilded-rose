607f05d7c66f917d4dbe9e9a26d27a00d8b651dd
Sam Livingston-Gray <geeksam@gmail.com>
2024-04-13 13:03:12 -0700

    [TCR] add parentheses around all conditional expressions

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,37 +1,37 @@
 require_relative 'lib'
 
 def update_quality(item)
-  if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-    if item.quality > 0
-      if item.name != 'Sulfuras, Hand of Ragnaros'
+  if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert')
+    if (item.quality > 0)
+      if (item.name != 'Sulfuras, Hand of Ragnaros')
         item.quality -= 1
       end
     end
   else
-    if item.quality < 50
+    if (item.quality < 50)
       item.quality += 1
-      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-        if item.sell_in < 11
-          if item.quality < 50
+      if (item.name == 'Backstage passes to a TAFKAL80ETC concert')
+        if (item.sell_in < 11)
+          if (item.quality < 50)
             item.quality += 1
           end
         end
-        if item.sell_in < 6
-          if item.quality < 50
+        if (item.sell_in < 6)
+          if (item.quality < 50)
             item.quality += 1
           end
         end
       end
     end
   end
-  if item.name != 'Sulfuras, Hand of Ragnaros'
+  if (item.name != 'Sulfuras, Hand of Ragnaros')
     item.sell_in -= 1
   end
-  if item.sell_in < 0
-    if item.name != "Aged Brie"
-      if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
+  if (item.sell_in < 0)
+    if (item.name != "Aged Brie")
+      if (item.name != 'Backstage passes to a TAFKAL80ETC concert')
+        if (item.quality > 0)
+          if (item.name != 'Sulfuras, Hand of Ragnaros')
             item.quality -= 1
           end
         end
@@ -39,7 +39,7 @@ def update_quality(item)
         item.quality = item.quality - item.quality
       end
     else
-      if item.quality < 50
+      if (item.quality < 50)
         item.quality += 1
       end
     end
