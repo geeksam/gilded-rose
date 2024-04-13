c1df02ae23b297f33fdc6c29b9a1cab4fe697938
Sam Livingston-Gray <geeksam@gmail.com>
2024-04-13 13:19:46 -0700

    [TCR] flatten nested conditionals
    Now that all of the if expressions have the same simple structures, I recorded a
    Vim macro to automate collapsing a three-line if..end expression into its
    parent.  Then I started from the bottom of the file, searched backward for the
    string 'if', and replayed the macro until everything was collapsed to the top
    level.

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,67 +1,29 @@
 require_relative 'lib'
 
 def update_quality(item)
-  if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert')
-    if (item.quality > 0)
-      if (item.name != 'Sulfuras, Hand of Ragnaros')
-        item.quality -= 1
-      end
-    end
+  if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') && (item.quality > 0) && (item.name != 'Sulfuras, Hand of Ragnaros')
+    item.quality -= 1
   end
-  if !(item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert')
-    if (item.quality < 50)
-      item.quality += 1
-    end
+  if !(item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') && (item.quality < 50)
+    item.quality += 1
   end
-  if !(item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert')
-    if (item.quality < 50)
-      if (item.name == 'Backstage passes to a TAFKAL80ETC concert')
-        if (item.sell_in < 11)
-          if (item.quality < 50)
-            item.quality += 1
-          end
-        end
-      end
-    end
+  if !(item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') && (item.quality < 50) && (item.name == 'Backstage passes to a TAFKAL80ETC concert') && (item.sell_in < 11) && (item.quality < 50)
+    item.quality += 1
   end
-  if !(item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert')
-    if (item.quality < 50)
-      if (item.name == 'Backstage passes to a TAFKAL80ETC concert')
-        if (item.sell_in < 6)
-          if (item.quality < 50)
-            item.quality += 1
-          end
-        end
-      end
-    end
+  if !(item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') && (item.quality < 50) && (item.name == 'Backstage passes to a TAFKAL80ETC concert') && (item.sell_in < 6) && (item.quality < 50)
+    item.quality += 1
   end
   if (item.name != 'Sulfuras, Hand of Ragnaros')
     item.sell_in -= 1
   end
-  if (item.sell_in < 0)
-    if (item.name != "Aged Brie")
-      if (item.name != 'Backstage passes to a TAFKAL80ETC concert')
-        if (item.quality > 0)
-          if (item.name != 'Sulfuras, Hand of Ragnaros')
-            item.quality -= 1
-          end
-        end
-      end
-    end
+  if (item.sell_in < 0) && (item.name != "Aged Brie") && (item.name != 'Backstage passes to a TAFKAL80ETC concert') && (item.quality > 0) && (item.name != 'Sulfuras, Hand of Ragnaros')
+    item.quality -= 1
   end
-  if (item.sell_in < 0)
-    if (item.name != "Aged Brie")
-      if !(item.name != 'Backstage passes to a TAFKAL80ETC concert')
-        item.quality = item.quality - item.quality
-      end
-    end
+  if (item.sell_in < 0) && (item.name != "Aged Brie") && !(item.name != 'Backstage passes to a TAFKAL80ETC concert')
+    item.quality = item.quality - item.quality
   end
-  if (item.sell_in < 0)
-    if !(item.name != "Aged Brie")
-      if (item.quality < 50)
-        item.quality += 1
-      end
-    end
+  if (item.sell_in < 0) && !(item.name != "Aged Brie") && (item.quality < 50)
+    item.quality += 1
   end
 end
 
